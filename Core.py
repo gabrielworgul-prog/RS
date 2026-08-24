325b019cea13950b1577994e3f3f4a26afd37010
gabrielworgul-prog <gabrielworgul@gmail.com>
2026-08-23 22:19:14 -0400

    Refactor sentence generation into think function
    Moved the random sentence generation into a new function called 'think'.

--- Core.py
+++ Core.py
@@ -1,5 +1,6 @@
 import random
-nouns = ["worker", "apointment", "speech", "cheek", "meat", "revenue", "college", "suggestion", "instance", "response"]
-adjectives = ["wise", "divergent", "worried", "bewildered", "deafening", "peaceful", "acoustic", "spiky", "complex", "bored"]
-verbs = ["pays", "contributes", "throws", "bids", "fires", "narrows", "keeps", "survives", "awards", "opposes"]
-print(f"The {random.choice(adjectives)} {random.choice(nouns)} {random.choice(verbs)} the {random.choice(adjectives)} {random.choice(nouns)}.")
+def think():
+  nouns = ["worker", "apointment", "speech", "cheek", "meat", "revenue", "college", "suggestion", "instance", "response"]
+  adjectives = ["wise", "divergent", "worried", "bewildered", "deafening", "peaceful", "acoustic", "spiky", "complex", "bored"]
+  verbs = ["pays", "contributes", "throws", "bids", "fires", "narrows", "keeps", "survives", "awards", "opposes"]
+  print(f"The {random.choice(adjectives)} {random.choice(nouns)} {random.choice(verbs)} the {random.choice(adjectives)} {random.choice(nouns)}.")
